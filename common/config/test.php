e021a3fb91431a787a516b089c46db652b637f76
afvillamizar <villamizarvecino@hotmail.com>
2019-10-16 18:19:00 -0500

    prueba bd

--- common/config/test.php
+++ common/config/test.php
@@ -7,5 +7,12 @@ return [
             'class' => 'yii\web\User',
             'identityClass' => 'common\models\User',
         ],
+		'db' => [
+		   'class' => 'yii\db\Connection',
+		   'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=integrada_test',
+		   'username' => 'postgres',
+		   'password' => '',
+		   'charset' => 'utf8',
+		],
     ],
 ];
